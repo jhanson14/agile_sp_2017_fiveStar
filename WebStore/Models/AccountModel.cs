05aee07d3124686a4894f0b7dcef72e82c19c7c5
jhanson14 <jack.hanson@live.com>
2020-02-12 15:55:09 -0600

    added more properties to AccountModel added more to the create account form

--- WebStore/Models/AccountModel.cs
+++ WebStore/Models/AccountModel.cs
@@ -5,7 +5,15 @@ namespace WebStore.Models
     {
         public String UserName { get; set; }
         public String Password { get; set; }
-    
-    
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Address { get; set; }
+
+        public String City { get; set; }
+        public String State { get; set; }
+        public String PostalCode { get; set; }
+
+
+
     }
 }

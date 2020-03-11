8a06ba1dfa4a86d3dfad15e273c9ab4892d1fd56
solson163 <solson163@student.cvtc.edu>
2020-03-11 12:54:24 -0500

    fixed buttons

--- WebStore/wwwroot/js/site.js
+++ WebStore/wwwroot/js/site.js
@@ -159,10 +159,10 @@ function displayCart() {
             //+ "<td><div class='product-image'><img src='" + cartArray[i].filePath + "'</td > "  NOTE: The img filePath is undefined
             + "<td>" + cartArray[i].name + "</td>"
             + "<td>($" + cartArray[i].price + " each)</td>"
-            + "<td><div class='input-group'><button class='minus-item input-group-addon btn btn-primary' data-name=" + cartArray[i].name + ">-</button>"
+            + "<td><div class='input-group'>"
             + "<input type='number' class='item-count form-control' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>"
-            + "<button class='plus-item btn btn-primary input-group-addon' data-name=" + cartArray[i].name + ">+</button></div></td>"
-            + "<td><button class='delete-item btn btn-danger' data-name=" + cartArray[i].name + ">X</button></td>"
+            + "</div></td>"
+            //+ "<td><button class='delete-item btn btn-danger' data-name=" + cartArray[i].name + ">X</button></td>"
             + " = "
             + "<td>" + cartArray[i].total + "</td>"
             + "</tr>";
